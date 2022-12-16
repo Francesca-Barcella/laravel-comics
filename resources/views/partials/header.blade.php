e318cf2649cef3268846ac006e6e1972886b3a71
Francesca-Barcella <pollon.fra@gmail>
2022-12-16 17:53:05 +0100

    header layout

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,54 @@
 <header>
-    <h3>testo header</h3>
+    <!-- header top -->
+    <div class="header_top bg-primary">
+        <div class="container d-flex justify-content-end text-white text-uppercase pt-2">
+            <h6 class="me-5">dc power visa</h6>
+            <h6>additionale dc sites ▼</h6>
+        </div>
+    </div>
+    <!-- header menu -->
+    <div class="header_menu">
+        <div class="container py-2 d-flex align-items-center justify-content-between center text-uppercase">
+            <!-- logo -->
+            <div>
+                <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="" class="logo">
+            </div>
+            <!-- navbar -->
+            <nav class="navbar navbar-expand-sm navbar-light">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">characters<span class="sr-only"></span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link px-3 text-primary" href="#">comics</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-3" href="#">movies</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-3" href="#">tv</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-3" href="#">games</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-3" href="#">collectibles</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-3" href="#">videos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-3" href="#">fan</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link px-3" href="#">shop <span class="text-primary">▼</span></a>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="search border-0 border-bottom border-secondary text-dark" type="text" placeholder="Search"> <span class="text-primary">⫯</span>
+                </form>
+            </nav>
+        </div>
+    </div>
 </header>
+
